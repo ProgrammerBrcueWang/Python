4da2a1a38d9433a5d90a53abb927d44728d40116
ProgrammerBrcueWang <45224893+ProgrammerBrcueWang@users.noreply.github.com>
2018-11-22 16:49:04 +0800

    Add files via upload

--- 参数的寻找.py
+++ 参数的寻找.py
@@ -5,13 +5,16 @@ c=456
 def foo():
 	def bar():
 		print("c=",c)    #内层函数没有定义c 但是他会从内层向外边函数中找，如果还找不到则会放到全局变量中找
-	bar()
+	bar()				 
 
 # 主函数
 
 if __name__ == '__main__':
 	foo()
+	hanshu = foo     # 给函数取别名
 	print(__name__)
 	# 查看内键变量
-	print(locals())  # 会返回一个字典
-	print(globals())
+	#print(locals())  # （工作空间所有的变量）会返回一个字典
+	#print(globals())
+	print(foo) # 返回函数的首地址
+	print(foo())  # python每个函数都有返回值 如果没有return 返回值是none
